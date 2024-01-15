cdb5962bec742744feb25fb6c7cb32c3acf9de74
lsuryana <lucaselbert@gmail.com>
2024-01-15 21:11:37 +0100

    add folder CommonErrors

---
+++ CommonErrors/main.cpp
@@ -0,0 +1,18 @@
+/* If you run this code you will definetly get an error. You need to fix the common erros for the code below. 
+You can see the solution in solution.cpp */
+
+int main ()
+{
+
+	x = 25
+	y = 61.4
+	z = 199.2
+
+	division = y / z
+
+	cout << x << '\n'
+	cout << y << '\n'
+	cout << division << '\n'
+	
+	return 0
+}
---
+++ CommonErrors/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main ()
+{
+	
+  	int x;
+  	float y;
+  	float z;
+	x = 25;
+	y = 61.4;
+	z = 199.2;
+
+  	float division;
+	division = y / z;
+
+	std::cout << x << '\n';
+	std::cout << y << '\n';
+	std::cout << division << '\n';
+	
+	return 0;
+}
